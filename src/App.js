5cabfc3b597c4b2c2781a60b9cfab918bf3495a3
jimram85 <jimram85@gmail.com>
2023-01-23 07:13:03 -0800

    restructured components, added custum button, implmented tailwind, proptypes,react icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import {GoBell, GoCloudDownload, GoDatabase} from 'react-icons/go';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    return <div>
+        <div>
+            <Button success rounded outline className="mb-5">
+            <GoBell />  
+            Click ME!
+            </Button>
+        </div>
+        <div>
+            <Button danger outline>
+            <GoCloudDownload />
+                Buy Now</Button>
+        </div>
+        <div>
+            <Button warning >
+            <GoDatabase />
+                See deals</Button>
+        </div>
+        <div>
+            <Button secondary outline>Checkout</Button>
+        </div>
+        <div>
+            <Button primary rounded>Primary</Button>
+        </div>
+       
+    </div>;
 }
 
 export default App;

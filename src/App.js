c63a4224b84edf550f95e744c4c9c8704baa754e
jimram85 <jimram85@gmail.com>
2023-01-26 07:01:00 -0800

    Added dropdown menu, restructured comps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import Accordion from "./components/Accordion";
+import { useState } from "react";
+import Dropdown from "./components/Dropdown";
 
-function App() {
-    const items = [
-        {
-            id:'dafgdgds',
-            label: 'Can I use  React on a project?',
-            content: 'You can use react on any project you want You can use react on any project you want You can use react on any project you want'
-        },
-        {
-            id:'sadsdiwdasd',
-            label:'Can I use Javascript on a project?',
-            content: 'You can use react on any project you want You can use react on any project you want You can use react on any project you want'
-        },
-        {
-            id: 'dsaodoejkd',
-            label:'Can i use CSS on a project?',
-            content: 'You can use react on any project you want You can use react on any project you want You can use react on any project you want'
-        }
-    ]
+function App(){
+    const [selection, setSelection] = useState(null);
     
-    
-    return <Accordion items = {items} />;
-}
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+   
+    const options = [
+    {label: 'Red', value: 'red'},
+    {label: 'Green', value: 'green'},
+    {label: 'Blue', value: 'blue'},
 
+   ];
+   
+   return (
+        <Dropdown 
+            options = {options} 
+            value = {selection}
+            onChange = {handleSelect} 
+        />
+   );
+    }
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,26 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+    const items = [
+        {
+            id:'dafgdgds',
+            label: 'Can I use  React on a project?',
+            content: 'You can use react on any project you want You can use react on any project you want You can use react on any project you want'
+        },
+        {
+            id:'sadsdiwdasd',
+            label:'Can I use Javascript on a project?',
+            content: 'You can use react on any project you want You can use react on any project you want You can use react on any project you want'
+        },
+        {
+            id: 'dsaodoejkd',
+            label:'Can i use CSS on a project?',
+            content: 'You can use react on any project you want You can use react on any project you want You can use react on any project you want'
+        }
+    ]
+    
+    
+    return <Accordion items = {items} />;
+}
+
+export default AccordionPage;
